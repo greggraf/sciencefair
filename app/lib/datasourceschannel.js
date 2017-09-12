00b7f79a8552837dcdaff58c56a1f297896e0a75
Greg Graf <me@greggraf.com>
2017-09-12 11:58:20 -0400

    add keys to identify and tie events/responses together

--- app/lib/datasourceschannel.js
+++ app/lib/datasourceschannel.js
@@ -1,100 +1,168 @@
-const getdatasource = require('../client/lib/getdatasource')
 const {ipcMain} = require('electron')
-
+const getdatasource = require('../client/lib/getdatasource')
 
 ipcMain.on('datasource:add', (event, source) => {
   getdatasource.fetch(source.key, (err, ds) => {
-    if (err) return event.sender.send('datasources:add:reply', err, {success: false})
+    if (err) return event.sender.send('datasource:add', source.key, err)
 
-    if (source.active) ds.setActive()    
-    ds.on('connected', event.sender.send('datasources:add:connected', 'pong'))
-    ds.on('progress', event.sender.send('datasources:add:progress', 'pong'))
+    if (source.active) ds.setActive()
+    ds.on('connected', () => event.sender.send('datasource:add:connected', source.key))
+    ds.on('progress', () => event.sender.send('datasource:add:progress', source.key))
 
     ds.connect()
 
     if (getdatasource.all().length > 1) {
-      event.sender.send('datasources:add:reply', null, {success: true, name: ds.name})
+      event.sender.send('datasource:add', source.key, null, ds.name)
     }
   })
 })
 
-ipcMain.on('datasource:toggleactive', (event, key) => {
-  getdatasource.fetch(key, (err, ds) => {
-    if (err) return event.sender.send('datasources:toggleactive:reply', err)
-    
-    ds.toggleActive()
-    
-    if (ds.stats.get('active').value()) {
-      ds.maybeSyncMetadata(err => {
-        if (err) return event.sender.send('datasources:toggleactive:reply', err)
-      })
-    }
-    
-    event.sender.send('datasources:toggleactive:reply')
+ipcMain.on('datasource:clear', (event, dskey, files, paperkey) => {
+  getdatasource.fetch(dskey, (err, ds) => {
+    if (err) return event.sender.send('datasource:clear', paperkey, err)
+
+    ds.clear(files, () => {
+     event.sender.send('datasource:clear', paperkey)
+    })
+
   })
 })
 
-ipcMain.on('datasource:download', (event, source) => {
-  getdatasource.fetch(key, (err, ds) => {
-    if (err) return event.sender.send('datasources:download:reply', err)
-    const download = ds.download(source)
-    if (!download) return null // datasource not ready
-    debug('downloading', source.key)
- 
-    download.on('progress', data => {
-      event.sender.send('datasources:download:progress', data)
-    })
+ipcMain.on('datasource:remove', (event, dskey) => {
+  getdatasource.fetch(dskey, (err, ds) => {
+    if (err) return event.sender.send('datasource:remove', dskey, err)
 
-    download.on('error', err => {
-      event.sender.send('datasources:download:error', err)
-    })
-
-    download.on('end', () => {
-      event.sender.send('datasources:download:end')
-    })
-
-    event.sender.send('datasources:download:reply')
+    ds.del(dskey)
+    event.sender.send('datasource:remove', dskey, err, ds.name)
   })
 })
 
-ipcMain.on('datasource:articlestats', (event, source) => {
-  getdatasource.fetch(source.key, (err, ds) => {
-    if (err) return event.sender.send('datasources:articlestats:reply', err)
-    
-    if (!(ds && ds.articles && ds.articles.content)) {
-      return event.sender.send('datasources:articlestats:reply', undefined, false)
-    }
-
-    ds.articlestats(source.files, (err, stats) => {
-      if (err) return event.sender.send('datasources:articlestats:reply', err)
-      event.sender.send('datasources:articlestats:reply', undefined, true, stats)
-    })
-
-    
-    event.sender.send('datasources:articlestats:reply')
-  })
-})
-
-ipcMain.on('datasource:clear', (event, key, files) => {
-  getdatasource.fetch(key, (err, ds) => {
-    if (err) return event.sender.send('datasources:clear:reply', err)
-    
-    ds.clear(files)
-    
-    event.sender.send('datasources:clear:reply')
-  })
-})
-
-ipcMain.on('datasource:ready', (event, key) => {
-  getdatasource.fetch(key, (err, ds) => {    
+ipcMain.on('datasource:ready', (event, dskey) => {
+  getdatasource.fetch(dskey, (err, ds) => {
     event.returnValue = ds.ready()
   })
 })
 
-ipcMain.on('datasources:getalldata', (event) => {
+ipcMain.on('datasource:toggleactive', (event, dskey) => {
+  getdatasource.fetch(dskey, (err, ds) => {
+    if (err) return event.sender.send('datasource:toggleactive', dskey, err)
+
+    ds.toggleActive()
+
+    if (ds.stats.get('active').value()) {
+      ds.maybeSyncMetadata(err => {
+        if (err) return event.sender.send('datasource:toggleactive', dskey, err)
+      })
+    }
+
+    event.sender.send('datasource:toggleactive', dskey)
+  })
+})
+
+ipcMain.on('datasource:getpaper', (event, paper) => {
+  getdatasource.fetch(paper.source, (err, ds) => {
+    if (err) {
+      return event.sender.send('datasource:getpaper', paper.key, err)
+    }
+
+    const download = ds.download(paper)
+    if (!download) {
+      return event.sender.send('datasource:getpaper', paper.key, "datastore not ready")
+    }
+
+    download.on('progress', data => {
+      event.sender.send('datasource:getpaper:progress', paper.key, data)
+    })
+
+    download.on('error', err => {
+      event.sender.send('datasource:getpaper', paper.key, err)
+    })
+
+    download.on('end', () => {
+      event.sender.send('datasource:getpaper:end', paper.key)
+    })
+
+    event.sender.send('datasource:getpaper', paper.key)
+  })
+})
+
+ipcMain.on('datasource:articlestats', (event, paper) => {
+  getdatasource.fetch(paper.source, (err, ds) => {
+    if (err) return event.sender.send('datasource:articlestats', paper.key, err)
+
+    if (!(ds && ds.articles && ds.articles.content)) {
+      return event.sender.send('datasource:articlestats', paper.key,  err, false)
+    }
+
+    ds.articlestats(paper.files, (err, stats) => {
+      if (err) return event.sender.send('datasource:articlestats', paper.key, err)
+      event.sender.send('datasource:articlestats', paper.key, err, true, stats)
+    })
+
+  })
+})
+
+ipcMain.on('datasource:getalldata', (event) => {
   event.returnValue = getdatasource.all().map(ds => ds.data())
 })
 
-ipcMain.on('datasources:updatespeeds', (event) => {
+ipcMain.on('datasource:getallspeeds', (event) => {
   event.returnValue = getdatasource.all().map(ds => ds.speed())
+})
+
+let activesearches = []
+
+ipcMain.on('datasource:search', (event, active, query) => {
+  const batchify = require('byte-stream')
+  const pumpify = require('pumpify')
+  const through = require('through2')
+
+  const resultify = ds => {
+    let count = 0
+
+    const write = (hits, _, cb) => {
+      count += hits.length
+
+      hits.forEach(r => {
+        r.source = ds.key
+      })
+      event.sender.send('datasource:search:receive', { hits: hits })
+      cb()
+    }
+
+    const flush = cb => {
+      if (count === 0) event.sender.send('datasource:search:receive', { hits: [] })
+      event.sender.send('datasource:search:results', { count: count, source: ds.name })
+      cb()
+    }
+
+    return through.obj(write, flush)
+  }
+
+  const cancelsearch = () => {
+    if (activesearches.length > 0) {
+      activesearches.forEach(resultstream => resultstream.destroy())
+      activesearches = []
+    }
+  }
+
+  ipcMain.on('datasource:search:cancel', (event) => {
+    cancelsearch()
+  })
+
+  cancelsearch()
+
+  active.forEach(ds => {
+    getdatasource.fetch(ds.key, (err, source) => {
+      if (err) return event.sender.send('datasource:search:error', err)
+
+        const resultstream = pumpify(
+          source.db.search(query),
+          batchify(30),
+          resultify(source)
+        )
+
+        activesearches.push(resultstream)
+    })
+  })
 })
